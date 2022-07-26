680607715f2fa07ac811075afbd560b723db17d7
arinzehills <arinzehill@gmail.com>
2022-07-27 00:41:24 +0100

    seeder

--- database/seeders/AdminSeeder.php
+++ database/seeders/AdminSeeder.php
@@ -14,13 +14,29 @@ class AdminSeeder extends Seeder
     public function run()
     {
         //
+        // \App\Models\User::create([
+        //     'firstname' => 'Napolean',
+        //     'lastname' => 'Okugbe',
+        //     'email' => 'support@conterize.com',
+        //     'password'=>bcrypt('123'),
+        //     'user_type' => 'admin',
+        //     'role_type' => 'admin',
+        // ]);
         \App\Models\User::create([
             'firstname' => 'Napolean',
             'lastname' => 'Okugbe',
-            'email' => 'support@conterize.com',
+            'email' => 'admin@conterize.com',
             'password'=>bcrypt('123'),
             'user_type' => 'admin',
             'role_type' => 'admin',
+        ]);
+        \App\Models\User::create([
+            'firstname' => 'James',
+            'lastname' => 'James',
+            'email' => 'bravediseo@gmail.com',
+            'password'=>bcrypt('123'),
+            'user_type' => 'business_user',
+            'payment_status'=>'paid',
         ]);
         // \App\Models\User::table('plans')->insert([
         //     'firstname' => 'Napolean',
