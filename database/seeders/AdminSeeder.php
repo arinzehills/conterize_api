78070b5473246dd94db068ebb8a758e559b385ae
arinzehills <arinzehill@gmail.com>
2022-07-28 19:22:22 +0100

    fixes

--- database/seeders/AdminSeeder.php
+++ database/seeders/AdminSeeder.php
@@ -14,38 +14,37 @@ class AdminSeeder extends Seeder
     public function run()
     {
         //
-        // \App\Models\User::create([
-        //     'firstname' => 'Napolean',
-        //     'lastname' => 'Okugbe',
-        //     'email' => 'support@conterize.com',
-        //     'password'=>bcrypt('123'),
-        //     'user_type' => 'admin',
-        //     'role_type' => 'admin',
-        // ]);
-        // \App\Models\User::create([
-        //     'firstname' => 'Napolean',
-        //     'lastname' => 'Okugbe',
-        //     'email' => 'admin@conterize.com',
-        //     'password'=>bcrypt('123'),
-        //     'user_type' => 'admin',
-        //     'role_type' => 'admin',
-        // ]);
-         \App\Models\User::create([
+        \App\Models\User::create([
+            'firstname' => 'Napolean',
+            'lastname' => 'Okugbe',
+            'email' => 'support@conterize.com',
+            'password'=>bcrypt('123'),
+            'user_type' => 'admin',
+            'role_type' => 'admin',
+        ]);
+        \App\Models\User::create([
+            'firstname' => 'Napolean',
+            'lastname' => 'Okugbe',
+            'email' => 'admin@conterize.com',
+            'password'=>bcrypt('123'),
+            'user_type' => 'admin',
+            'role_type' => 'admin',
+        ]);
+        \App\Models\User::create([
+            'firstname' => 'James',
+            'lastname' => 'James',
+            'email' => 'bravediseo@gmail.com',
+            'password'=>bcrypt('123'),
+            'user_type' => 'business_user',
+            'payment_status'=>'paid',
+        ]);
+        \App\Models\User::create([
             'firstname' => 'achills',
             'lastname' => 'Chris',
             'email' => 'developer@conterize.com',
-            'password'=>strtolower('Scale'),
+            'password'=>bcrypt('123'),
             'user_type' => 'admin',
             'role_type' => 'developer',
         ]);
-        
-        // \App\Models\User::table('plans')->insert([
-        //     'firstname' => 'achills',
-        //     'lastname' => 'tech',
-        //     'email' => 'developer@conterize.com',
-        //     'password'=>strtolower('Scale'),
-        //     'user_type' => 'admin',
-        //     'role_type' => 'developer',
-        // ]);
     }
 }
